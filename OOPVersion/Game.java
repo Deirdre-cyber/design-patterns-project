4d583f45a78542c2a1286001f915e8c7fd39a3bd
Deirdre-cyber <74724725+Deirdre-cyber@users.noreply.github.com>
2021-11-23 20:57:51 +0000

    GUI started

--- OOPVersion/Game.java
+++ OOPVersion/Game.java
@@ -4,6 +4,8 @@ public interface Game {
 
     //Mental Block converting from procedural console to OOP GUI - starting over.......
 
+
+    //Rethink all this
     void choosePlayers();
     void chooseGameVersion();
     char[] createCode(String play);
