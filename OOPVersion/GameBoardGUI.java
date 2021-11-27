eb2575cb6da7cba021b47468c8945378956237a4
Deirdre-cyber <74724725+Deirdre-cyber@users.noreply.github.com>
2021-11-27 21:03:57 +0000

    GUI update

--- OOPVersion/GameBoardGUI.java
+++ OOPVersion/GameBoardGUI.java
@@ -4,18 +4,25 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.util.ArrayList;
 
-public class GameBoardGUI extends JFrame /*, KeyListener*/ {
+public class GameBoardGUI extends JFrame {
 
     private JButton[] guessButtons;
-    private JPanel colourButtonPanel, playPanel;
-    private int guessEventCount=0;
+    private JButton[] colourButtons;
+    private JPanel[] guessPanels;
+    private JPanel[] hintPanels;
+    private JPanel mainBoard, gameBoard, guessPanel, colourButtonPanel, playPanel;
+    private JLabel solutionLabelLeft, solutionLabelRight, gameCounterLabel, hintsLabel;
+    private ArrayList<Color> buttonColourList;  //HIDDEN BUTTON
+    private JButton makeGuess, blankColour; //preferably add to colourButtons - make arrayLIst
+    private int guessEventCount = 0; //make static?
+    private Font gameFont = new Font("Monospaced", Font.PLAIN, 22);
 
     public GameBoardGUI(){
-        super("MASTERMIND");
+        super("Welcome Player");
 
-        JPanel mainBoard = new JPanel();
+        mainBoard = new JPanel();
         mainBoard.setLayout(new GridLayout(1,2));   //make better with gridbaglayout
 
         mainBoard.add(createGameBoard());
@@ -29,26 +36,146 @@ public class GameBoardGUI extends JFrame /*, KeyListener*/ {
         setResizable(false);
         setVisible(true);
     }
-    //GETTERS AND SETTERS
+    //GETTERS AND SETTERS - DELETE ALL UNNECESSARY!!!
+    public JButton[] getGuessButtons() {
+        return guessButtons;
+    }
+    public void setGuessButtons(JButton[] guessButtons) {
+        this.guessButtons = guessButtons;
+    }
 
+    public JButton[] getColourButtons() {
+        return colourButtons;
+    }
+    public void setColourButtons(JButton[] colourButtons) {
+        this.colourButtons = colourButtons;
+    }
+
+    public JPanel[] getGuessPanels() {
+        return guessPanels;
+    }
+    public void setGuessPanels(JPanel[] guessPanels) {
+        this.guessPanels = guessPanels;
+    }
+
+    public JPanel[] getHintPanels() {
+        return hintPanels;
+    }
+    public void setHintPanels(JPanel[] hintPanels) {
+        this.hintPanels = hintPanels;
+    }
+
+    public JPanel getMainBoard() {
+        return mainBoard;
+    }
+    public void setMainBoard(JPanel mainBoard) {
+        this.mainBoard = mainBoard;
+    }
+
+    public JPanel getGameBoard() {
+        return gameBoard;
+    }
+    public void setGameBoard(JPanel gameBoard) {
+        this.gameBoard = gameBoard;
+    }
+
+    public JPanel getGuessPanel() {
+        return guessPanel;
+    }
+    public void setGuessPanel(JPanel guessPanel) {
+        this.guessPanel = guessPanel;
+    }
+
+    public JPanel getColourButtonPanel() {
+        return colourButtonPanel;
+    }
+    public void setColourButtonPanel(JPanel colourButtonPanel) {
+        this.colourButtonPanel = colourButtonPanel;
+    }
+
+    public JPanel getPlayPanel() {
+        return playPanel;
+    }
+    public void setPlayPanel(JPanel playPanel) {
+        this.playPanel = playPanel;
+    }
+
+    public JLabel getSolutionLabelLeft() {
+        return solutionLabelLeft;
+    }
+    public void setSolutionLabelLeft(JLabel solutionLabelLeft) {
+        this.solutionLabelLeft = solutionLabelLeft;
+    }
+
+    public JLabel getSolutionLabelRight() {
+        return solutionLabelRight;
+    }
+    public void setSolutionLabelRight(JLabel solutionLabelRight) {
+        this.solutionLabelRight = solutionLabelRight;
+    }
+
+    public JLabel getGameCounterLabel() {
+        return gameCounterLabel;
+    }
+    public void setGameCounterLabel(JLabel gameCounterLabel) {
+        this.gameCounterLabel = gameCounterLabel;
+    }
+
+    public JLabel getHintsLabel() {
+        return hintsLabel;
+    }
+    public void setHintsLabel(JLabel hintsLabel) {
+        this.hintsLabel = hintsLabel;
+    }
+
+    public ArrayList<Color> getButtonColourList() {
+        return buttonColourList;
+    }
+    public void setButtonColourList(ArrayList<Color> buttonColourList) {
+        this.buttonColourList = buttonColourList;
+    }
+
+    public JButton getMakeGuess() {
+        return makeGuess;
+    }
+    public void setMakeGuess(JButton makeGuess) {
+        this.makeGuess = makeGuess;
+    }
+
+    public JButton getBlankColour() {
+        return blankColour;
+    }
+    public void setBlankColour(JButton blankColour) {
+        this.blankColour = blankColour;
+    }
+
+    public int getGuessEventCount() {
+        return guessEventCount;
+    }
+    public void setGuessEventCount(int guessEventCount) {
+        this.guessEventCount = guessEventCount;
+    }
+
+    public Font getGameFont() {
+        return gameFont;
+    }
 
     private JPanel createGameBoard(){
 
-        JPanel gameBoard = new JPanel(new GridLayout(1, 2));
+        gameBoard = new JPanel(new GridLayout(1, 2));
 
         //RIGHT PANEL
         JPanel panelLeft = new JPanel(new GridLayout(12, 1));
-        //panelLeft.setSize(300, 500);
         panelLeft.setBackground(Color.GRAY);
 
-        JLabel solutionLabel = new JLabel("MASTER");//change to solution graphic if game won : if-else
-        solutionLabel.setFont(new Font("Monospaced", Font.BOLD, 22));
-        solutionLabel.setForeground(Color.WHITE);
-        solutionLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+        solutionLabelLeft = new JLabel("MASTER");//change to solution graphic if game won : if-else
+        solutionLabelLeft.setFont(gameFont);
+        solutionLabelLeft.setForeground(Color.WHITE);
+        solutionLabelLeft.setHorizontalAlignment(SwingConstants.RIGHT);
 
-        panelLeft.add(solutionLabel);
+        panelLeft.add(solutionLabelLeft);
 
-        JPanel[] guessPanels = new JPanel[10]; //Number is numGames chosen
+        guessPanels = new JPanel[10]; //Number is numGuesses chosen
 
         for(int i = 0; i < guessPanels.length; i++){
             guessPanels[i] = new JPanel(new FlowLayout());
@@ -58,24 +185,24 @@ public class GameBoardGUI extends JFrame /*, KeyListener*/ {
             //add graphics - 4 circle lines - to each panel...new class or method : paintComponent
         }
 
-        JLabel gameCounter = new JLabel("Game No.1"); //set by numGames : after game play "You win/you lose"
-        gameCounter.setFont(new Font("Monospaced", Font.PLAIN, 18));
-        gameCounter.setForeground(Color.WHITE);
-        gameCounter.setHorizontalAlignment(SwingConstants.CENTER);
+        gameCounterLabel = new JLabel("Game No.1"); //set by numGames : after game play "You win/you lose"
+        gameCounterLabel.setFont(gameFont);
+        gameCounterLabel.setForeground(Color.WHITE);
+        gameCounterLabel.setHorizontalAlignment(SwingConstants.CENTER);
 
-        panelLeft.add(gameCounter);
+        panelLeft.add(gameCounterLabel);
 
         //RIGHT PANEL
         JPanel panelRight = new JPanel(new GridLayout(12, 1));
         panelRight.setBackground(Color.GRAY);
 
-        JLabel solutionLabel2 = new JLabel("MIND");
-        solutionLabel2.setFont(new Font("Monospaced", Font.BOLD, 22));
-        solutionLabel2.setForeground(Color.WHITE);
-        solutionLabel2.setHorizontalAlignment(SwingConstants.LEFT);
-        panelRight.add(solutionLabel2);
+        solutionLabelRight = new JLabel("MIND");        //use GridBagLayout?
+        solutionLabelRight.setFont(gameFont);
+        solutionLabelRight.setForeground(Color.WHITE);
+        solutionLabelRight.setHorizontalAlignment(SwingConstants.LEFT);
+        panelRight.add(solutionLabelRight);
 
-        JPanel[] hintPanels = new JPanel[10]; //Number is numGames chosen
+        hintPanels = new JPanel[10]; //Number is numGames chosen
 
         for(int i = 0; i < hintPanels.length; i++){
             hintPanels[i] = new JPanel(new FlowLayout());
@@ -85,8 +212,8 @@ public class GameBoardGUI extends JFrame /*, KeyListener*/ {
             //add graphics - 4 circle lines - to each panel...new class or method : paintComponent
         }
 
-        JLabel hintsLabel = new JLabel("hints"); //set by numGames
-        hintsLabel.setFont(new Font("Monospaced", Font.PLAIN, 16));
+        hintsLabel = new JLabel("hints"); //set by numGames
+        hintsLabel.setFont(gameFont);
         hintsLabel.setForeground(Color.WHITE);
         hintsLabel.setHorizontalAlignment(SwingConstants.CENTER);
         panelRight.add(hintsLabel);
@@ -107,13 +234,22 @@ public class GameBoardGUI extends JFrame /*, KeyListener*/ {
         colourButtonPanel = new JPanel(new GridLayout(4, 2));
         colourButtonPanel.setBorder(BorderFactory.createMatteBorder(8, 8, 8, 8, Color.WHITE));
 
-        JButton[] colourButtons = new JButton[8];
-        Color[] buttonColours = {Color.WHITE, Color.YELLOW, Color.GREEN, Color.RED, Color.BLUE, Color.PINK, Color.CYAN, Color.ORANGE};
+        colourButtons = new JButton[8];
 
-        for(int i = 0; i < colourButtons.length; i++){
+        buttonColourList = new ArrayList<>();
+        buttonColourList.add(Color.WHITE);
+        buttonColourList.add(Color.YELLOW);
+        buttonColourList.add(Color.GREEN);
+        buttonColourList.add(Color.RED);
+        buttonColourList.add(Color.BLUE);
+        buttonColourList.add(Color.PINK);
+        buttonColourList.add(Color.CYAN);
+        buttonColourList.add(Color.ORANGE);
+
+        for(int i = 0; i < buttonColourList.size(); i++){
             colourButtons[i] = new JButton();
-            colourButtons[i].setBackground(buttonColours[i]);
-            colourButtons[i].setForeground(buttonColours[i]);
+            colourButtons[i].setBackground(buttonColourList.get(i));
+            colourButtons[i].setForeground(buttonColourList.get(i));
             colourButtons[i].setBorder(BorderFactory.createMatteBorder(2,2,2,2, Color.GRAY));
             colourButtons[i].addMouseListener(new MouseAdapter() {
                 @Override
@@ -125,19 +261,24 @@ public class GameBoardGUI extends JFrame /*, KeyListener*/ {
                         System.out.print(guessEventCount);
                         guessEventCount = 0;
                     }
-
                     guessEventCount++;
                 }
             });
             colourButtonPanel.add(colourButtons[i]);
         }
 
+        /*//add hidden button code
+        blankColour = new JButton("[blank]");
+        blankColour.setBackground(Color.GRAY);
+        blankColour.setForeground(Color.WHITE);
+        colourButtonPanel.add(blankColour);*/
+
         playPanel.add(colourButtonPanel);
 
         JLabel placeholder2 = new JLabel("  ");
         playPanel.add(placeholder2);
 
-        JPanel guessPanel = new JPanel(new GridBagLayout());
+        guessPanel = new JPanel(new GridBagLayout());
         guessPanel.setBackground(Color.GRAY);
         guessButtons = new JButton[4];
 
@@ -156,14 +297,13 @@ public class GameBoardGUI extends JFrame /*, KeyListener*/ {
                 }
             });
             guessPanel.add(guessButtons[i]);
-            //add listeners if pressed button resets colour
         }
 
         playPanel.add(guessPanel);
 
-        JButton makeGuess = new JButton("Make Guess");
+        makeGuess = new JButton("Make Guess");
         makeGuess.setBackground(Color.WHITE);
-        makeGuess.setFont(new Font("Monospaced", Font.BOLD, 18));
+        makeGuess.setFont(gameFont);
         makeGuess.setBackground(Color.GRAY);
         makeGuess.setForeground(Color.WHITE);
         makeGuess.setBorder(BorderFactory.createMatteBorder(8,8,8,8,Color.WHITE));
@@ -172,6 +312,7 @@ public class GameBoardGUI extends JFrame /*, KeyListener*/ {
             public void mousePressed(MouseEvent e) {
                 if(guessEventCount == 4){      //guess event must be correct
                     JOptionPane.showMessageDialog(null, "Guess made");
+                    guessPanels[1].add(guessPanel);
                     //increment guess counter etc
                 }
                 else
@@ -183,5 +324,77 @@ public class GameBoardGUI extends JFrame /*, KeyListener*/ {
         playPanel.add(makeGuess);
 
         return playPanel;
+    }
+
+    protected static JButton createQuitButton(){
+
+        JButton quitButton = new JButton("[Q]UIT");
+        quitButton.setFont(new Font("Monospaced", Font.BOLD, 18)); //getFont from gui
+        quitButton.setBackground(Color.BLUE);
+        quitButton.setForeground(Color.BLACK);
+        quitButton.setBorder(BorderFactory.createMatteBorder(10,10,10,10,Color.GRAY));
+
+        quitButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                System.exit(0);
+            }
+        });
+
+        return quitButton;
+    }
+
+    protected static JButton createStartButton(){
+
+        JButton startButton = new JButton("[S]TART NEW GAME");
+        startButton.setFont(new Font("Monospaced", Font.BOLD, 18)); //getFont from gui somehow?? before game is loaded....
+        startButton.setBackground(Color.ORANGE);
+        startButton.setForeground(Color.BLACK);
+        startButton.setBorder(BorderFactory.createMatteBorder(10,10,10,10,Color.GRAY));
+
+        startButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                GameBoardGUI newGame = new GameBoardGUI();
+            }
+        });
+
+        return startButton;
+    }
+    protected static JButton createLoadButton(){
+
+        JButton loadButton = new JButton("[L]OAD GAME");
+        loadButton.setFont(new Font("Monospaced", Font.BOLD, 18)); //getFont from gui
+        loadButton.setBackground(Color.GREEN);
+        loadButton.setForeground(Color.BLACK);
+        loadButton.setBorder(BorderFactory.createMatteBorder(10,10,10,10,Color.GRAY));
+
+        loadButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                GameBoardGUI newGame = new GameBoardGUI();
+                //open saved game files
+            }
+        });
+
+        return loadButton;
+    }
+    public static JButton createViewLeaderboardButton(){
+
+        JButton viewLeaderBoardButton = new JButton("[V]IEW LEADERBOARD");
+        viewLeaderBoardButton.setFont(new Font("Monospaced", Font.BOLD, 18)); //getFont from gui
+        viewLeaderBoardButton.setBackground(Color.CYAN);
+        viewLeaderBoardButton.setForeground(Color.BLACK);
+        viewLeaderBoardButton.setBorder(BorderFactory.createMatteBorder(10,10,10,10,Color.GRAY));
+
+        viewLeaderBoardButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                System.out.print("View Leaderboard");
+                //open saves files
+            }
+        });
+
+        return viewLeaderBoardButton;
     }
 }
--- OOPVersion/Mastermind.java
+++ OOPVersion/Mastermind.java
@@ -1,9 +1,50 @@
 package OOPVersion;
 
-public class Mastermind {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Mastermind extends JFrame {
+
     public static void main(String[] args) {
 
-        GameBoardGUI newGame = new GameBoardGUI();
+        JFrame game = new JFrame("MASTERMIND");
 
+        JPanel startGamePanel = new JPanel(new GridLayout(4, 1));
+        game.add(startGamePanel);
+
+        startGamePanel.add(GameBoardGUI.createStartButton());
+        startGamePanel.add(GameBoardGUI.createLoadButton());
+        startGamePanel.add(GameBoardGUI.createViewLeaderboardButton());
+        startGamePanel.add(GameBoardGUI.createQuitButton());
+
+        startGamePanel.setFocusable(true);
+
+        //how to implement key adapter in definition header again??
+        startGamePanel.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if(e.getKeyCode() == KeyEvent.VK_S){
+                    GameBoardGUI newGame = new GameBoardGUI();
+                }
+                else if(e.getKeyCode() == KeyEvent.VK_L){
+                    GameBoardGUI newGame = new GameBoardGUI();
+                    //load new game
+                }
+                else if(e.getKeyCode() == KeyEvent.VK_V){
+                    System.out.print("Load Leaderboard");
+                }
+                else if(e.getKeyCode() == KeyEvent.VK_Q){
+                    System.exit(0);
+                }
+            }
+        });
+
+        game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        game.setLocation(550, 50);
+        game.setSize(400,400);
+        game.setResizable(false);
+        game.setVisible(true);
     }
+
 }
